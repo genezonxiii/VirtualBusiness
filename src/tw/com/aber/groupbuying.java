53b48a31b608a097b467f60176599c0243e11e54
Ian <qaz7456@gmail.com>
2017-04-24 09:55:54 +0800

    團購轉檔作業
    整理後端code

--- src/tw/com/aber/groupbuying.java
+++ src/tw/com/aber/groupbuying.java
@@ -42,7 +42,7 @@ import com.google.gson.Gson;
 public class groupbuying extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	private static final Logger logger = LogManager.getLogger(upload.class);
+	private static final Logger logger = LogManager.getLogger(groupbuying.class);
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
