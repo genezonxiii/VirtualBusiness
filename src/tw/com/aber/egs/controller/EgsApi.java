68c4a0649d184c73931c127556f988ab1848a9ef
TIBYBY\kevin_chan <ciss00217@gmail.com>
2017-07-24 15:18:25 +0800

    新增分支develop_report
    新增電文功能 getQuerySuda7,getEGSNum

--- src/tw/com/aber/egs/controller/EgsApi.java
+++ src/tw/com/aber/egs/controller/EgsApi.java
@@ -1,7 +1,10 @@
 package tw.com.aber.egs.controller;
 
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
@@ -16,7 +19,58 @@ public class EgsApi {
 	private EgsApi egsApi;
 	private String conString = "http://192.168.112.156:8800/egs?";
 	private String cmd = "cmd=";
+	
+	public Map toMap(String string) {
+		Map<String, String> map = new HashMap<String, String>();
+		String[] pairs = string.split("&");
+		if (pairs.length > 0) {
+			for (int i = 0; i < pairs.length; i++) {
+				String pair = pairs[i];
+				String[] keyValue = pair.split("=");
+				map.put(keyValue[0], keyValue[1]);
+			}
+		}
+		return map;
+	}
 
+	public String getQuerySuda7(String address) {
+		String suda7 = null;
+		try {
+			address = URLEncoder.encode(address, "utf8");
+
+			String sendString = "query_suda7&address_1=" + address;
+			logger.debug("sendString:" + sendString);
+
+			String responseStr = send(sendString);
+
+			logger.debug("responseStr:" + responseStr);
+			Map<String, String> map = toMap(responseStr);
+			suda7 = map.get("suda7_1");
+
+		} catch (Exception e) {
+			logger.debug("error:" + e.getMessage());
+			return "";
+		}
+		if (suda7 != null) {
+			suda7 = suda7.replace("+", "");
+			return suda7;
+		} else {
+			return "";
+		}
+	}
+
+	public String getEGSNum() {
+		String responseStr = send("query_egs_info");
+		logger.debug("responseStr:" + responseStr);
+		Map<String, String> map = toMap(responseStr);
+		String eGSNum = map.get("egs_version");
+		if (eGSNum != null) {
+			return eGSNum;
+		} else {
+			return "";
+		}
+	}
+	
 	public String send(String command) {
 		egsApi = new EgsApi();
 		cmd = cmd.concat(command);
