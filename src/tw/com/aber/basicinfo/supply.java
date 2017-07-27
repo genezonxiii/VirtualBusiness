a71fb9c7587b83f3b32930b4f58deecd36b44e1b
TIBYBY\kevin_chan <ciss00217@gmail.com>
2017-07-27 15:55:09 +0800

    供應商管理
    新增:查詢供應商採購紀錄功能

--- src/tw/com/aber/basicinfo/supply.java
+++ src/tw/com/aber/basicinfo/supply.java
@@ -20,7 +20,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import tw.com.aber.vo.PurchaseDetailVO;
+import tw.com.aber.vo.PurchaseVO;
 import tw.com.aber.vo.SupplyVO;
 
 
@@ -43,10 +46,14 @@ public class supply extends HttpServlet {
 		String group_id = request.getSession().getAttribute("group_id").toString();
 		String user_id = request.getSession().getAttribute("user_id").toString();
 		
+		logger.debug("action: "+action);
+		logger.debug("group_id: "+group_id);
+		
 		if ("search".equals(action)) {
 			try {
 				/*************************** 1.接收請求參數 ****************************************/
 				String supply_name = request.getParameter("supply_name");
+				logger.debug("supply_name: "+supply_name);
 				/*************************** 2.開始查詢資料 ****************************************/
 				// 假如無查詢條件，則是查詢全部
 				if (supply_name == null || (supply_name.trim()).length() == 0) {
@@ -83,6 +90,21 @@ public class supply extends HttpServlet {
 				String mobile1 = request.getParameter("mobile1");
 				String memo = request.getParameter("memo");
 				
+				logger.debug("supply_name: "+supply_name);
+				logger.debug("supply_unicode: "+supply_unicode);
+				logger.debug("address: "+address);
+				logger.debug("contact: "+contact);
+				logger.debug("phone: "+phone);
+				logger.debug("ext: "+ext);
+				logger.debug("mobile: "+mobile);
+				logger.debug("contact1: "+contact1);
+				logger.debug("phone1: "+phone1);
+				logger.debug("ext1: "+ext1);
+				logger.debug("email: "+email);
+				logger.debug("email1: "+email1);
+				logger.debug("mobile1: "+mobile1);
+				logger.debug("memo: "+memo);
+				
 				/*************************** 2.開始新增資料 ***************************************/
 				supplyVOService = new SupplyVOService();
 				supplyVOService.addSupply(group_id, supply_name, supply_unicode, address, contact,phone, ext, mobile,
@@ -116,6 +138,22 @@ public class supply extends HttpServlet {
 				String email = request.getParameter("email");
 				String email1 = request.getParameter("email1");
 				String memo = request.getParameter("memo");
+				
+				logger.debug("supply_id: "+supply_id);
+				logger.debug("supply_name: "+supply_name);
+				logger.debug("supply_unicode: "+supply_unicode);
+				logger.debug("address: "+address);
+				logger.debug("contact: "+contact);
+				logger.debug("phone: "+phone);
+				logger.debug("ext: "+ext);
+				logger.debug("mobile: "+mobile);
+				logger.debug("contact1: "+contact1);
+				logger.debug("phone1: "+phone1);
+				logger.debug("ext1: "+ext1);
+				logger.debug("email: "+email);
+				logger.debug("email1: "+email1);
+				logger.debug("mobile1: "+mobile1);
+				logger.debug("memo: "+memo);
 				/*************************** 2.開始修改資料 ***************************************/
 				supplyVOService = new SupplyVOService();
 				supplyVOService.updateSupply(supply_id, group_id, supply_name, supply_unicode, address, contact, phone,
@@ -135,6 +173,7 @@ public class supply extends HttpServlet {
 			try {
 				/*************************** 1.接收請求參數 ***************************************/
 				String supply_id = request.getParameter("supply_id");
+				logger.debug("supply_id: "+supply_id);
 				/*************************** 2.開始刪除資料 ***************************************/
 				supplyVOService = new SupplyVOService();
 				supplyVOService.deleteSupply(supply_id,user_id);
@@ -149,6 +188,36 @@ public class supply extends HttpServlet {
 				e.printStackTrace();
 			}
 		}
+		
+		if("getPurchaseRecordBySupplyId".equals(action)){
+			try {
+				String supply_id = request.getParameter("supply_id");
+				logger.debug("supply_id: "+supply_id);
+				
+				supplyVOService = new SupplyVOService();
+				List<PurchaseVO> list=supplyVOService.getPurchaseRecordBySupplyId(group_id,supply_id);
+
+				Gson gson =  new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+				String jsonStrList = gson.toJson(list);
+				response.getWriter().write(jsonStrList);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// 採購明細
+		if ("select_all_purchasedetail".equals(action)) {
+			String purchase_id = request.getParameter("purchase_id");
+			logger.debug("purchase_id: "+purchase_id);
+			
+			supplyVOService = new SupplyVOService();
+			List<PurchaseDetailVO> list = supplyVOService.getSearchAllPurchaseDetail(purchase_id);
+
+			Gson gson =  new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+			String jsonStrList = gson.toJson(list);
+			response.getWriter().write(jsonStrList);
+			return;// 程式中斷
+		}
 	}
 
 	/*************************** 制定規章方法 ****************************************/
@@ -156,11 +225,15 @@ public class supply extends HttpServlet {
 
 		public void insertDB(SupplyVO supply);
 
+		List<PurchaseDetailVO> searchAllPurchaseDetail(String purchase_id);
+
 		public void updateDB(SupplyVO supply);
 
 		public void deleteDB(String supply_id,String user_id);
 
 		public List<SupplyVO> searchAllDB(String group_id);
+		
+		public List<PurchaseVO> getPurchaseRecordBySupplyId(String group_id,String supply_id);
 		
 	}
 
@@ -171,7 +244,10 @@ public class supply extends HttpServlet {
 		public SupplyVOService() {
 			dao = new SupplyDAO();
 		}
-
+		public List<PurchaseVO> getPurchaseRecordBySupplyId(String group_id,String supply_id){
+			return dao.getPurchaseRecordBySupplyId(group_id, supply_id);
+		}
+		
 		public SupplyVO addSupply(String group_id, String supply_name,String supply_unicode,String address,String contact,String phone,
 				String ext,String mobile,String contact1,String phone1,String ext1,String mobile1,String email,String email1,String memo,String user_id) {
 			SupplyVO supplybean = new SupplyVO();
@@ -228,6 +304,9 @@ public class supply extends HttpServlet {
 		public List<SupplyVO> getSearchAllDB(String group_id) {
 			return dao.searchAllDB(group_id);
 		}
+		public List<PurchaseDetailVO> getSearchAllPurchaseDetail(String purchase_id) {
+			return dao.searchAllPurchaseDetail(purchase_id);
+		}
 		
 	}
 
@@ -238,7 +317,10 @@ public class supply extends HttpServlet {
 		private static final String sp_selectall_supply = "call sp_selectall_supply (?)";
 		private static final String sp_del_supply = "call sp_del_supply (?,?)";
 		private static final String sp_update_supply = "call sp_update_supply (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-		
+		private static final String sp_get_purchase_record_by_supply_id = "call sp_get_purchase_record_by_supply_id (?,?)";
+		private static final String sp_selectall_purchasedetail = "call sp_selectall_purchasedetail(?)";
+
+
 		private final String dbURL = getServletConfig().getServletContext().getInitParameter("dbURL")
 				+ "?useUnicode=true&characterEncoding=utf-8&useSSL=false";
 		private final String dbUserName = getServletConfig().getServletContext().getInitParameter("dbUserName");
@@ -460,6 +542,139 @@ public class supply extends HttpServlet {
 			}
 			return list;
 		}
+
+		@Override
+		public List<PurchaseVO> getPurchaseRecordBySupplyId(String group_id, String supply_id) {
+
+			List<PurchaseVO> list = new ArrayList<PurchaseVO>();
+			PurchaseVO purchaseVO = null;
+
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				con = DriverManager.getConnection(dbURL, dbUserName, dbPassword);
+				pstmt = con.prepareStatement(sp_get_purchase_record_by_supply_id);
+				pstmt.setString(1, group_id);
+				pstmt.setString(2, supply_id);
+				rs = pstmt.executeQuery();
+				while (rs.next()) {
+					purchaseVO = new PurchaseVO();
+					purchaseVO.setPurchase_id(rs.getString("purchase_id"));
+					purchaseVO.setSeq_no(rs.getString("seq_no"));
+					purchaseVO.setGroup_id(rs.getString("group_id"));
+					purchaseVO.setUser_id(rs.getString("user_id"));
+					purchaseVO.setSupply_id(rs.getString("supply_id"));
+					purchaseVO.setMemo(rs.getString("memo"));
+					purchaseVO.setPurchase_date(rs.getDate("purchase_date"));
+					purchaseVO.setInvoice(rs.getString("invoice"));
+					purchaseVO.setInvoice_type(rs.getString("invoice_type"));
+					purchaseVO.setAmount(rs.getFloat("amount"));
+					purchaseVO.setReturn_date(rs.getDate("return_date"));
+					purchaseVO.setIsreturn(rs.getBoolean("isreturn"));
+					purchaseVO.setAccept_flag(rs.getBoolean("accept_flag"));
+					purchaseVO.setV_supply_name(rs.getString("supply_name"));
+					
+					list.add(purchaseVO); // Store the row in the list
+				}
+				// Handle any driver errors
+			} catch (SQLException se) {
+				throw new RuntimeException("A database error occured. " + se.getMessage());
+			} catch (ClassNotFoundException cnfe) {
+				throw new RuntimeException("A database error occured. " + cnfe.getMessage());
+				// Clean up JDBC resources
+			} finally {
+				if (rs != null) {
+					try {
+						rs.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (con != null) {
+					try {
+						con.close();
+					} catch (Exception e) {
+						e.printStackTrace(System.err);
+					}
+				}
+			}
+			return list;
+		}
+		
+		@Override
+		public List<PurchaseDetailVO> searchAllPurchaseDetail(String purchase_id) {
+
+			List<PurchaseDetailVO> list = new ArrayList<PurchaseDetailVO>();
+			PurchaseDetailVO purchaseDetailVO = null;
+
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				con = DriverManager.getConnection(dbURL, dbUserName, dbPassword);
+				pstmt = con.prepareStatement(sp_selectall_purchasedetail);
+				pstmt.setString(1, purchase_id);
+				rs = pstmt.executeQuery();
+				while (rs.next()) {
+					purchaseDetailVO = new PurchaseDetailVO();
+					purchaseDetailVO.setPurchaseDetail_id(rs.getString("purchaseDetail_id"));
+					purchaseDetailVO.setPurchase_id(rs.getString("purchase_id"));
+					purchaseDetailVO.setGroup_id(rs.getString("group_id"));
+					purchaseDetailVO.setUser_id(rs.getString("user_id"));
+					purchaseDetailVO.setProduct_id(rs.getString("product_id"));
+					purchaseDetailVO.setC_product_id(rs.getString("c_product_id"));
+					purchaseDetailVO.setProduct_name(rs.getString("product_name"));
+					purchaseDetailVO.setQuantity(rs.getInt("quantity"));
+					purchaseDetailVO.setCost(rs.getFloat("cost"));
+					purchaseDetailVO.setMemo(rs.getString("memo"));
+					purchaseDetailVO.setReturn_date(rs.getDate("return_date"));
+					purchaseDetailVO.setIsreturn(rs.getBoolean("isreturn"));
+					list.add(purchaseDetailVO);
+				}
+				// Handle any driver errors
+			} catch (SQLException se) {
+				throw new RuntimeException("A database error occured. " + se.getMessage());
+			} catch (ClassNotFoundException cnfe) {
+				throw new RuntimeException("A database error occured. " + cnfe.getMessage());
+				// Clean up JDBC resources
+			} finally {
+				if (rs != null) {
+					try {
+						rs.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (con != null) {
+					try {
+						con.close();
+					} catch (Exception e) {
+						e.printStackTrace(System.err);
+					}
+				}
+			}
+			return list;
+		}
+
 		
 	}
 
